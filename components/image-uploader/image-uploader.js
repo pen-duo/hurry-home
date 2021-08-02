d9f00918e104c9dc59dfad179afe77e3a9371dcd
pengyiduo <2411363896@qq.com>
2021-08-02 20:14:14 +0800

    feat: 基本完成发布页面表单项展示功能

---
+++ components/image-uploader/image-uploader.js
@@ -0,0 +1,19 @@
+// components/image-uploader/image-uploader.js
+Component({
+  properties: {
+    // 默认展示图片文件
+    files: {
+      type: Array,
+      value: []
+    },
+    maxCount: {
+      type: Number
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
--- components/service-form/service-form.js
+++ components/service-form/service-form.js
@@ -1,5 +1,7 @@
 import serviceType from "../../enum/service-type"
-import { getEventParams } from "../../utils/utils"
+import Category from "../../modal/category"
+import { getDataSet, getEventParams } from "../../utils/utils"
+import regeneratorRuntime from "../../lib/runtime/runtime"
 
 Component({
   properties: {
@@ -26,7 +28,9 @@ Component({
       begin_date: "",
       end_date: "",
       price: ""
-    }
+    },
+    categoryList: [],
+    categoryPickerIndex: null
   },
   lifetimes: {
     attached() {
@@ -34,10 +38,12 @@ Component({
     }
   },
   methods: {
-    _init() {
-      const index = this.data.typeList.findIndex(item => this.form.type === item.id)
+    async _init() {
+      const typePickerIndex = this.data.typeList.findIndex(item => this.data.form.type === item.id)
+      const categoryList = await Category.getCategoryList()
+      const categoryPickerIndex = categoryList.findIndex(item => this.data.form.category_id === item.id)
       this.setData({
-        typePickerIndex: index !== -1 ? index : null,
+        typePickerIndex: typePickerIndex !== -1 ? typePickerIndex : null,
         formData: {
           type: this.data.form.type,
           title: this.data.form.title,
@@ -48,8 +54,13 @@ Component({
           begin_date: this.data.form.begin_date,
           end_date: this.data.form.end_date,
           price: this.data.form.price
-        }
+        },
+        categoryPickerIndex: categoryPickerIndex !== -1 ? categoryPickerIndex : null,
+        categoryList,
       })
+    },
+    submit() {
+      console.log(this.data.formData);
     },
     handleTypeChange(e) {
       const index = getEventParams(e, "value")
@@ -57,6 +68,38 @@ Component({
         typePickerIndex: index,
         ['formData.type']: this.data.typeList[index].id
       })
-    }
+    },
+    handleInput(e) {
+      const value = getEventParams(e, "value")
+      const field = getDataSet(e, "field")
+      this.setData({
+        [`formData.${field}`]: value
+      })
+    },
+    handleCategoryChange(e) {
+      const index = getEventParams(e, "value")
+      this.setData({
+        categoryPickerIndex: index,
+        ["formData.category_id"]: this.data.categoryList[index].id
+      })
+    },
+    handleSwitchChange(e) {
+      const res = getEventParams(e, "value")
+      this.setData({
+        ["formData.designated_place"]: res
+      })
+    },
+    handleBeginDateChange(e) {
+      const beginDate = getEventParams(e, "value")
+      this.setData({
+        ["formData.begin_date"]: beginDate
+      })
+    },
+    handleEndDateChange(e) {
+      const endDate = getEventParams(e, "value")
+      this.setData({
+        ["formData.end_date"]: endDate
+      })
+    },
   }
 })
