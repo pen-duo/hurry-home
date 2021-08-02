da0acc502eada11f2c7e642e63295892a2e55eaf
pengyiduo <2411363896@qq.com>
2021-08-02 19:48:12 +0800

    feat: 发布类型表单项实现

--- components/service-form/service-form.js
+++ components/service-form/service-form.js
@@ -15,7 +15,18 @@ Component({
         name: "找服务"
       }
     ],
-    typePickerIndex: null
+    typePickerIndex: null,
+    formData: {
+      type: null,
+      title: "",
+      category_id: null,
+      cover_image_id: null,
+      description: "",
+      designated_place: false,
+      begin_date: "",
+      end_date: "",
+      price: ""
+    }
   },
   lifetimes: {
     attached() {
@@ -26,13 +37,25 @@ Component({
     _init() {
       const index = this.data.typeList.findIndex(item => this.form.type === item.id)
       this.setData({
-        typePickerIndex: index !== -1 ? index : null
+        typePickerIndex: index !== -1 ? index : null,
+        formData: {
+          type: this.data.form.type,
+          title: this.data.form.title,
+          category_id: this.data.form.category_id,
+          cover_image_id: this.data.form.cover_image_id,
+          description: this.data.form.description,
+          designated_place: this.data.form.designated_place,
+          begin_date: this.data.form.begin_date,
+          end_date: this.data.form.end_date,
+          price: this.data.form.price
+        }
       })
     },
     handleTypeChange(e) {
       const index = getEventParams(e, "value")
       this.setData({
-        typePickerIndex: index
+        typePickerIndex: index,
+        ['formData.type']: this.data.typeList[index].id
       })
     }
   }
